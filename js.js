b5ae3496f580ef9a9911c79df37fa8a929e2916c
unknown <chafikaim@gmail.com>
2022-04-15 11:59:23 +0200

    le project est on cours de modification

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 
 
-class TICtoc{
+export default class TICtoc{
 
 
     constructor(container){
@@ -13,8 +13,12 @@ class TICtoc{
         this.p1=this.container.parentNode.querySelector('.player1');
         this.p2=this.container.parentNode.querySelector('.player2');
         this.alert=this.container.parentNode.querySelector('.alert');
+        this.gameover=this.container.parentNode.querySelector('.gameover');
+        this.succes=this.container.parentNode.querySelector('.succes');
         this.player1=false;
         this.player2=false;
+        this.restart=this.gameover.querySelector('.game');
+        this.restart2=this.succes.querySelector('.game');
 
         // les delete des notification:
         this.span1=this.p1.querySelector('span');
@@ -38,9 +42,17 @@ class TICtoc{
             this.delete(this.span2);
         }.bind(this));
         this.span3.addEventListener('click',function(e){
-            this.delete(this.alert);
+            this.delete(this.span3);
         }.bind(this));
 
+        
+        this.restart.addEventListener('click',function(e){
+            location.reload();
+        });
+
+        this.restart2.addEventListener('click',function(e){
+            location.reload();
+        });
     }
 
     /**
@@ -70,27 +82,39 @@ class TICtoc{
             this.click='O';
         }
     }
-
     verify=(tab)=>{
         if(!this.player1 && !this.player2 ){
         this.gangne.forEach(ele => {
             if(tab[ele[0]] === 'X' && tab[ele[1]] === 'X' && tab[ele[2]] === 'X' ){
                 this.player1 = true;
                 this.p1.classList.add('active');
+                // success player 1
+                this.succes.classList.add('fade')
+                this.succes.classList.add('on');
             }else if(tab[ele[0]] === 'O' && tab[ele[1]] === 'O' && tab[ele[2]] === 'O'){
                 this.player2=true;
                 this.p2.classList.add('active');
-            }else if(this.count === 8 && this.player1 === false && this.player2 ===false){
-                this.alert.classList.add('active');
-                this.count=0;
+
+                // succes player 2
+                this.succes.classList.add('fade')
+                this.succes.classList.add('on');
+            }else{
+                if(this.count === 71 & !this.player1 && !this.player2){
+                    this.alert.classList.add('active');
+                  console.log(this.gameover)
+                    this.gameover.classList.add('fade');
+                    if(this.gameover.classList.contains('fade')){
+                    this.gameover.addEventListener('transitionend',function(){
+                        this.classList.add('on');
+                        console.log('this');
+                    })
+                    }
                 }
+                this.count+=1;
+            }
         })
         }
-        this.count++;
-
     }
-
-
     // delete des notification
     delete=(element)=>{
       if(element.parentNode.classList.contains('active')){
@@ -100,6 +124,3 @@ class TICtoc{
       }
     }
 }
-
-let container=document.querySelector('.container');
-let tic=new TICtoc(container);
